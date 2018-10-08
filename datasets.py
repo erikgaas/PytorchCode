18d566af010ebfc9f9c2dccf844e24624156e860
Erik Gaasedelen <egaasedelen@lyft>
2018-10-07 23:13:02 -0700

    change

--- datasets.py
+++ datasets.py
@@ -6,7 +6,7 @@ import torch
 from albumentations import Resize, NoOp, Normalize, Compose
 from albumentations import Flip, RandomRotate90
 
-dihedral = Compose(Flip(), RandomRotate90())
+dihedral = Compose([Flip(), RandomRotate90()])
 
 def open_image(img_path, tfm=NoOp()):
     img = cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2RGB)
